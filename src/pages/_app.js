4b14846daa71ce14e464981cb477be547d63fe14
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-22 15:02:53 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,13 @@
 // src/pages/_app.js
+
 import "@styles/globals.css";
 import "@styles/theme.css";
-import Layout from "@components/Layout";
-import { MagicLinkProvider } from "@contexts/MagicLinkContext";
+import { MagicLinkProvider } from "@/contexts/MagicLinkContext";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <MagicLinkProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </MagicLinkProvider>
   );
 }
