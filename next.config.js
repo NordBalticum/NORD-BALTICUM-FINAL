b037975cd8722f1ceb61ff8e2133708380158c64
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-22 14:22:26 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
+const path = require("path");
+
 module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
+  webpack: (config) => {
+    config.resolve.alias["@components"] = path.resolve(__dirname, "src/components");
+    config.resolve.alias["@styles"] = path.resolve(__dirname, "src/styles");
+    config.resolve.alias["@contexts"] = path.resolve(__dirname, "src/contexts");
+    config.resolve.alias["@lib"] = path.resolve(__dirname, "src/lib");
+    return config;
+  },
 };
