ad989db7e8678fd3875ed2a3df7c016ad8a80bab
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-22 13:36:28 +0200

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useMagicLink } from "../contexts/MagicLinkContext";
+import { useMagicLink } from "@contexts/MagicLinkContext";
 import { useState } from "react";
-import styles from "./navbar.module.css";
+import styles from "@components/navbar.module.css";
 
 export default function NavBar() {
   const { signOut } = useMagicLink();
