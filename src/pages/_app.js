56ffd1c290aef80c7b51fbcda22e620f19e0bd4b
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-22 19:54:34 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,14 @@
+// src/pages/_app.js
+
 import "@/styles/globals.css";
 import "@/styles/theme.css";
 import "@/styles/fullscreen-ui.css";
-
 import { useAutoScale } from "@/hooks/useAutoScale";
 import { MagicLinkProvider } from "@/contexts/MagicLinkContext";
 
 export default function MyApp({ Component, pageProps }) {
-  useAutoScale(); // Default 1440x900, scale 0.44–1
+  useAutoScale(); // ✅ natūralus zoom efektas be iškraipymų
+
   return (
     <MagicLinkProvider>
       <Component {...pageProps} />
