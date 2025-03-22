bcd657caea6d06c19400e1567f2f7ef7725f012f
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-22 18:08:21 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
 // src/pages/_app.js
 
-import "@styles/globals.css";
-import "@styles/theme.css";
+import "@/styles/globals.css";
+import "@/styles/theme.css";
+import "@/styles/fullscreen-ui.css"; // ✅ Global fullscreen layout
 import { MagicLinkProvider } from "@/contexts/MagicLinkContext";
 
 export default function MyApp({ Component, pageProps }) {
