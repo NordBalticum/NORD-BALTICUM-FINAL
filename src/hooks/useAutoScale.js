366d4187a6a68a1a7ee60fd084073d81c934e714
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-22 19:25:24 +0200

    Update useAutoScale.js

--- src/hooks/useAutoScale.js
+++ src/hooks/useAutoScale.js
@@ -1,5 +1,11 @@
 import { useEffect } from "react";
 
+/**
+ * Automatinis `--scale-factor` nustatymas pagal ekrano dydį.
+ * @param {number} baseWidth - dizaino bazinis plotis (default 1440px)
+ * @param {number} min - minimalus scale
+ * @param {number} max - maksimalus scale
+ */
 export function useAutoScale(baseWidth = 1440, min = 0.44, max = 1) {
   useEffect(() => {
     const setScale = () => {
