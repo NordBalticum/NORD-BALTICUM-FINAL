c65ae2a5f06fef2bd732c36c616b4041ec893ae4
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-24 04:32:07 +0200

    Create history.js

---
+++ src/pages/history.js
@@ -0,0 +1,10 @@
+import styles from '@/history.module.css';
+
+export default function YourPage() {
+  return (
+    <div className={styles.container}>
+      <h1>Coming Soon</h1>
+      <p>This page is reserved for future development.</p>
+    </div>
+  );
+}
