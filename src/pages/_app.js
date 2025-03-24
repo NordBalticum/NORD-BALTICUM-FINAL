ff98a31b9bea4e88f18d0eedb4044fe98d96f2ae
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-24 07:38:47 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,22 @@
+"use client";
+
 import "@/styles/globals.css";
 import "@/styles/theme.css";
 import { MagicLinkProvider } from "@/contexts/MagicLinkContext";
 import { BalanceProvider } from "@/contexts/BalanceContext";
 import Layout from "@/components/Layout";
 import Head from "next/head";
+import React from "react";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <React.StrictMode>
       <Head>
         <title>NordBalticum</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, user-scalable=no"
+        />
         <meta name="theme-color" content="#0A1F44" />
         <link rel="icon" href="/icons/logo.png" />
       </Head>
@@ -22,6 +28,6 @@ export default function App({ Component, pageProps }) {
           </Layout>
         </BalanceProvider>
       </MagicLinkProvider>
-    </>
+    </React.StrictMode>
   );
 }
