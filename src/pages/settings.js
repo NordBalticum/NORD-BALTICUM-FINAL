746dd81bb3f895969000fdec67c2bddb11f1b5cb
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-24 04:33:24 +0200

    Create settings.js

---
+++ src/pages/settings.js
@@ -0,0 +1,10 @@
+import styles from '@/settings.module.css';
+
+export default function YourPage() {
+  return (
+    <div className={styles.container}>
+      <h1>Coming Soon</h1>
+      <p>This page is reserved for future development.</p>
+    </div>
+  );
+}
