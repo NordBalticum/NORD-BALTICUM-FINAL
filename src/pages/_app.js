7c25fcc0f6dfe90331d86f2a2c10cca9655cd849
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-25 13:57:18 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@
 import "@/styles/globals.css";
 import "@/styles/theme.css";
 import { MagicLinkProvider } from "@/contexts/MagicLinkContext";
+import { WebAuthnProvider } from "@/contexts/WebAuthnContext";
 import { BalanceProvider } from "@/contexts/BalanceContext";
 import Layout from "@/components/Layout";
 import Head from "next/head";
@@ -20,19 +21,22 @@ export default function App({ Component, pageProps }) {
         <meta name="theme-color" content="#0A1F44" />
         <link rel="icon" href="/icons/logo.png" />
 
-        {/* Crypto šriftas */}
+        {/* Custom font */}
         <link
           href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap"
           rel="stylesheet"
         />
       </Head>
 
+      {/* Providers */}
       <MagicLinkProvider>
-        <BalanceProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </BalanceProvider>
+        <WebAuthnProvider>
+          <BalanceProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </BalanceProvider>
+        </WebAuthnProvider>
       </MagicLinkProvider>
     </React.StrictMode>
   );
