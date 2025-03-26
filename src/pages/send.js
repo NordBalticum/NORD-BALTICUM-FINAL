aa258bb77d5dcabae8eef29caba9a66c7cf05bcb
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-27 01:25:21 +0200

    Update send.js

--- src/pages/send.js
+++ src/pages/send.js
@@ -4,7 +4,6 @@
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useMagicLink } from "@/contexts/MagicLinkContext";
-import { createClient } from "@supabase/supabase-js";
 import { ethers } from "ethers";
 import BottomNavigation from "@/components/BottomNavigation";
 import styles from "@/styles/swipe.module.css";
