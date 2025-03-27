9951082d79d193963766687365c3757e65ce1955
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-27 03:45:14 +0200

    Update AvatarPicker.js

--- src/components/AvatarPicker.js
+++ src/components/AvatarPicker.js
@@ -1,37 +1,48 @@
-"use client";
+import React, { useState } from "react";
+import styles from "./avatarpicker.module.css";
 
-import React, { useEffect, useState } from "react";
-import styles from "@/components/avatarpicker.module.css";
+const avatarList = [
+  "/avatars/avatar1.png",
+  "/avatars/avatar2.png",
+  "/avatars/avatar3.png",
+  "/avatars/avatar4.png",
+  "/avatars/avatar5.png",
+  "/avatars/avatar6.png",
+  "/avatars/avatar7.png",
+  "/avatars/avatar8.png",
+  "/avatars/avatar9.png",
+  "/avatars/avatar10.png",
+  "/avatars/avatar11.png",
+  "/avatars/avatar12.png",
+  "/avatars/avatar13.png",
+  "/avatars/avatar14.png",
+  "/avatars/avatar15.png",
+  "/avatars/avatar16.png",
+  "/avatars/avatar17.png",
+  "/avatars/avatar18.png",
+  "/avatars/avatar19.png",
+  "/avatars/avatar20.png",
+];
 
-const avatars = Array.from({ length: 20 }, (_, i) => `/avatars/avatar${i + 1}.png`);
+export default function AvatarPicker({ onSelect }) {
+  const [selected, setSelected] = useState(avatarList[0]);
 
-export default function AvatarPicker() {
-  const [selected, setSelected] = useState("/avatars/avatar1.png");
-
-  useEffect(() => {
-    const stored = localStorage.getItem("profile_avatar");
-    if (stored) setSelected(stored);
-  }, []);
-
-  const handleSelect = (url) => {
-    localStorage.setItem("profile_avatar", url);
-    setSelected(url);
+  const handlePick = (avatar) => {
+    setSelected(avatar);
+    onSelect(avatar);
   };
 
   return (
-    <div className={styles.avatarPicker}>
-      <div className={styles.current}>
-        <img src={selected} alt="Selected Avatar" />
-        <p>Click any avatar to update</p>
-      </div>
+    <div className={styles.wrapper}>
+      <img src={selected} className={styles.mainAvatar} alt="Selected Avatar" />
       <div className={styles.grid}>
-        {avatars.map((url, index) => (
+        {avatarList.map((avatar) => (
           <img
-            key={index}
-            src={url}
-            alt={`Avatar ${index + 1}`}
-            className={`${styles.avatar} ${selected === url ? styles.selected : ""}`}
-            onClick={() => handleSelect(url)}
+            key={avatar}
+            src={avatar}
+            alt="avatar"
+            className={`${styles.avatar} ${selected === avatar ? styles.active : ""}`}
+            onClick={() => handlePick(avatar)}
           />
         ))}
       </div>
