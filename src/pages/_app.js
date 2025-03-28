889d6920c9434f708826f52c20b8ed8cf6c477a1
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-28 03:13:37 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,36 +1,21 @@
 import "@/styles/globals.css";
 import "@/styles/theme.css";
 import React from "react";
-import Head from "next/head";
 
 import { MagicLinkProvider } from "@/contexts/MagicLinkContext";
 import { WalletLoadProvider } from "@/contexts/WalletLoadContext";
 import { BalanceProvider } from "@/contexts/BalanceContext";
-import { GenerateWalletProvider } from "@/contexts/GenerateWalletContext"; // <- PRIDĖTA
-import { AuthProvider } from "@/contexts/AuthContext"; // <- sujungia viską
+import { GenerateWalletProvider } from "@/contexts/GenerateWalletContext";
+import { AuthProvider } from "@/contexts/AuthContext";
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
     <React.StrictMode>
-      <Head>
-        <title>NordBalticum</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, user-scalable=no"
-        />
-        <meta name="theme-color" content="#0A1F44" />
-        <link rel="icon" href="/icons/logo.png" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
       <MagicLinkProvider>
         <WalletLoadProvider>
           <BalanceProvider>
-            <GenerateWalletProvider> {/* <- PRIDĖTA */}
+            <GenerateWalletProvider>
               <AuthProvider>
                 <Layout>
                   <Component {...pageProps} />
