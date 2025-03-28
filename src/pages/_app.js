632611ef59684078e8383a07642b200eaee5a4a8
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-28 18:46:32 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,5 @@
+"use client";
+
 import "@/styles/globals.css";
 import "@/styles/theme.css";
 import React from "react";
@@ -7,6 +9,7 @@ import { WalletLoadProvider } from "@/contexts/WalletLoadContext";
 import { BalanceProvider } from "@/contexts/BalanceContext";
 import { GenerateWalletProvider } from "@/contexts/GenerateWalletContext";
 import { AuthProvider } from "@/contexts/AuthContext";
+
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
