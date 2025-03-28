af32b14d0eb6b8784f86cd86ae74278fc509ac35
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-29 01:22:41 +0200

    Update MagicLinkContext.js

--- src/contexts/MagicLinkContext.js
+++ src/contexts/MagicLinkContext.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { supabase } from "@/lib/supabaseClient";
+import { supabase } from "@/lib/supabase";
 
 const MagicLinkContext = createContext();
 
