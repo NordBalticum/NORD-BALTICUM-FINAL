f32cdea234a77df2ec4712e9862431544abbf2b4
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-29 00:37:01 +0200

    Create networks.js

---
+++ src/utils/networks.js
@@ -0,0 +1,29 @@
+// utils/networks.js
+
+export const supportedNetworks = [
+  {
+    name: "BNB Chain",
+    symbol: "bnb",
+    logo: "https://cryptologos.cc/logos/binance-coin-bnb-logo.png",
+  },
+  {
+    name: "BNB Testnet",
+    symbol: "tbnb",
+    logo: "https://cryptologos.cc/logos/binance-coin-bnb-logo.png",
+  },
+  {
+    name: "Ethereum",
+    symbol: "eth",
+    logo: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
+  },
+  {
+    name: "Polygon",
+    symbol: "pol",
+    logo: "https://cryptologos.cc/logos/polygon-matic-logo.png",
+  },
+  {
+    name: "Avalanche",
+    symbol: "avax",
+    logo: "https://cryptologos.cc/logos/avalanche-avax-logo.png",
+  },
+];
