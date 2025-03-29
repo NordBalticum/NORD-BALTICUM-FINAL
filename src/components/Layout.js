fb311616c84f762cf449005255b78518b1e2f0fa
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-03-29 12:09:35 +0200

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
 import { usePathname } from "next/navigation";
-
 import SideDrawer from "@/components/SideDrawer";
 import BottomNav from "@/components/BottomNavigation";
 import styles from "@/components/layout.module.css";
@@ -11,17 +9,12 @@ export default function Layout({ children }) {
   const pathname = usePathname();
   const hideUI = pathname === "/";
 
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
-    return () => {
-      document.body.style.overflow = "auto";
-    };
-  }, []);
-
   return (
     <div className={styles.layoutWrapper}>
       {!hideUI && <SideDrawer />}
-      <main className={styles.mainContent}>{children}</main>
+      <div className={styles.mainContent}>
+        {children}
+      </div>
       {!hideUI && <BottomNav />}
     </div>
   );
