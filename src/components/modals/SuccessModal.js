37a4f825cf3df7fed55fcd2ccb8adb320de02b01
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-04-02 00:13:24 +0300

    Update SuccessModal.js

--- src/components/modals/SuccessModal.js
+++ src/components/modals/SuccessModal.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import styles from "@/styles/successmodal.module.css";
+import styles from "./successmodal.module.css";
 
 const explorers = {
   bnb: "https://bscscan.com/tx/",
