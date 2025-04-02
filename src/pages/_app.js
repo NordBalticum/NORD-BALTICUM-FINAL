698cbd2b2febca6ca3238868803c50e0cf6fa103
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-04-03 02:43:40 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,17 +9,13 @@ import { WalletProvider } from "@/contexts/WalletContext";
 import { BalanceProvider } from "@/contexts/BalanceContext";
 import { SendCryptoProvider } from "@/contexts/SendCryptoContext";
 
-import Layout from "@/components/Layout";
-
 export default function App({ Component, pageProps }) {
   return (
     <MagicLinkProvider>
       <WalletProvider>
         <BalanceProvider>
           <SendCryptoProvider>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
+            <Component {...pageProps} />
           </SendCryptoProvider>
         </BalanceProvider>
       </WalletProvider>
