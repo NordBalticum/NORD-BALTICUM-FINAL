67e72a1acc7449b52b6e2cbd959de4f42e0ba148
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-04-02 13:36:18 +0300

    Update BottomNavigation.js

--- src/components/BottomNavigation.js
+++ src/components/BottomNavigation.js
@@ -10,6 +10,7 @@ import {
   FaClock,
   FaUserCircle,
 } from "react-icons/fa";
+
 import styles from "@/components/bottomnav.module.css";
 
 const navItems = [
@@ -24,13 +25,19 @@ export default function BottomNavigation() {
   const pathname = usePathname();
 
   return (
-    <nav className={styles.navbar} role="navigation" aria-label="Bottom Navigation">
+    <nav
+      className={styles.navbar}
+      role="navigation"
+      aria-label="Bottom Navigation"
+    >
       {navItems.map(({ path, icon, label }) => {
         const isActive = pathname === path;
+
         return (
-          <Link key={path} href={path}>
+          <Link key={path} href={path} legacyBehavior>
             <a
               className={`${styles.navItem} ${isActive ? styles.active : ""}`}
+              aria-current={isActive ? "page" : undefined}
               aria-label={label}
             >
               <div className={styles.icon}>{icon}</div>
