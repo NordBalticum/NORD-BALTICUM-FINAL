668d03083f862e6b2e77c70737c650208a36b5aa
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-04-02 19:09:17 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,17 +5,14 @@ import "@/styles/theme.css";
 import "@/styles/globals.css";
 
 import { MagicLinkProvider } from "@/contexts/MagicLinkContext";
-import { WalletProvider } from "@/contexts/WalletContext";
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
     <MagicLinkProvider>
-      <WalletProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </WalletProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </MagicLinkProvider>
   );
 }
