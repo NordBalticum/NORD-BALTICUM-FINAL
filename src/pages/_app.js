04c9f7f58f6141d51d806884618382963785a406
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-04-04 22:15:05 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,17 @@
-"use client";
+"use client";  
 
-import "@/styles/theme.css";
-import "@/styles/globals.css";
+import "@/styles/theme.css";  
+import "@/styles/globals.css";  
 
-import { AuthProvider } from "@/contexts/AuthContext"; // Vienintelis variklis!
+import { AuthProvider } from "@/contexts/AuthContext"; // Vienintelis variklis!  
+import Layout from "@/components/Layout"; // Importuojame Layout komponentą
 
-export default function App({ Component, pageProps }) {
-  return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
-  );
+export default function App({ Component, pageProps }) {  
+  return (  
+    <AuthProvider>  
+      <Layout> {/* Apvyniojame Layout komponentu */}
+        <Component {...pageProps} />  
+      </Layout>  
+    </AuthProvider>  
+  );  
 }
