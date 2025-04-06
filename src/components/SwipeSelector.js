ce156815cd65a927a080547433e40d2687086e07
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-04-07 00:39:46 +0300

    Update SwipeSelector.js

--- src/components/SwipeSelector.js
+++ src/components/SwipeSelector.js
@@ -9,10 +9,10 @@ import styles from "@/components/swipeselector.module.css";
 
 const supportedNetworks = [
   { name: "BNB Testnet", symbol: "tbnb", logo: "https://cryptologos.cc/logos/binance-coin-bnb-logo.png" },
-  { name: "BNB Chain", symbol: "bnb", logo: "https://cryptologos.cc/logos/binance-coin-bnb-logo.png" },
-  { name: "Ethereum", symbol: "eth", logo: "https://cryptologos.cc/logos/ethereum-eth-logo.png" },
-  { name: "Polygon", symbol: "matic", logo: "https://cryptologos.cc/logos/polygon-matic-logo.png" },
-  { name: "Avalanche", symbol: "avax", logo: "https://cryptologos.cc/logos/avalanche-avax-logo.png" },
+  { name: "BNB Chain", symbol: "bsc", logo: "https://cryptologos.cc/logos/binance-coin-bnb-logo.png" },
+  { name: "Ethereum", symbol: "ethereum", logo: "https://cryptologos.cc/logos/ethereum-eth-logo.png" },
+  { name: "Polygon", symbol: "polygon", logo: "https://cryptologos.cc/logos/polygon-matic-logo.png" },
+  { name: "Avalanche", symbol: "avalanche", logo: "https://cryptologos.cc/logos/avalanche-avax-logo.png" },
 ];
 
 export default function SwipeSelector({ onSelect }) {
