c7e26850102f5d49c5b6fb20a65c60672a152915
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-04-10 08:55:38 +0300

    Update networkApi.js

--- src/utils/networkApi.js
+++ src/utils/networkApi.js
@@ -1,27 +1,39 @@
 "use client";
 
-const API_KEYS = {
-  eth: process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY,
-  bnb: process.env.NEXT_PUBLIC_BSCSCAN_API_KEY,
-  tbnb: process.env.NEXT_PUBLIC_TBSCSCAN_API_KEY,
-  matic: process.env.NEXT_PUBLIC_POLYGONSCAN_API_KEY,
-  avax: "", // Avalanche neturi API key
-};
+export const fetchNetworkTransactions = async (network, address) => {
+  const API_KEYS = {
+    bnb: process.env.NEXT_PUBLIC_BSCSCAN_API_KEY,
+    tbnb: process.env.NEXT_PUBLIC_TBSCSCAN_API_KEY,
+    eth: process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY,
+    polygon: process.env.NEXT_PUBLIC_POLYGONSCAN_API_KEY,
+    avax: "", // Avalanche neturi raktų
+  };
 
-export const networkApi = {
-  eth: {
-    url: (address) => `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=desc&apikey=${API_KEYS.eth}`,
-  },
-  bnb: {
-    url: (address) => `https://api.bscscan.com/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=desc&apikey=${API_KEYS.bnb}`,
-  },
-  tbnb: {
-    url: (address) => `https://api-testnet.bscscan.com/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=desc&apikey=${API_KEYS.tbnb}`,
-  },
-  matic: {
-    url: (address) => `https://api.polygonscan.com/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=desc&apikey=${API_KEYS.matic}`,
-  },
-  avax: {
-    url: (address) => `https://api.snowtrace.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=desc&apikey=`, // Avax nemeta error net be key
-  },
+  const API_URLS = {
+    bnb: "https://api.bscscan.com/api",
+    tbnb: "https://api-testnet.bscscan.com/api",
+    eth: "https://api.etherscan.io/api",
+    polygon: "https://api.polygonscan.com/api",
+    avax: "https://api.snowtrace.io/api",
+  };
+
+  const url = new URL(API_URLS[network]);
+  url.searchParams.append("module", "account");
+  url.searchParams.append("action", "txlist");
+  url.searchParams.append("address", address);
+  url.searchParams.append("startblock", "0");
+  url.searchParams.append("endblock", "99999999");
+  url.searchParams.append("sort", "desc");
+  if (API_KEYS[network]) {
+    url.searchParams.append("apikey", API_KEYS[network]);
+  }
+
+  const response = await fetch(url.toString());
+  const data = await response.json();
+
+  if (data.status !== "1") {
+    throw new Error(data.message || "Failed to fetch transactions");
+  }
+
+  return data.result || [];
 };
