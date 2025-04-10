552b72f3afff250c920f4a3e60e83be076bfa20f
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-04-10 04:08:17 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,19 @@
-"use client";  
+"use client";
 
-import "@/styles/theme.css";  
-import "@/styles/globals.css";  
+import "@/styles/theme.css";
+import "@/styles/globals.css";
 
-import { AuthProvider } from "@/contexts/AuthContext"; // Vienintelis variklis!  
+import { AuthProvider } from "@/contexts/AuthContext"; // Vienintelis variklis!
 import Layout from "@/components/Layout"; // Importuojame Layout komponentą
+import { Toaster } from "react-hot-toast"; // Pridėti react-hot-toast
 
-export default function App({ Component, pageProps }) {  
-  return (  
-    <AuthProvider>  
+export default function App({ Component, pageProps }) {
+  return (
+    <AuthProvider>
       <Layout> {/* Apvyniojame Layout komponentu */}
-        <Component {...pageProps} />  
-      </Layout>  
-    </AuthProvider>  
-  );  
+        <Toaster position="top-right" reverseOrder={false} /> {/* Reikia čia įdėti Toaster */}
+        <Component {...pageProps} />
+      </Layout>
+    </AuthProvider>
+  );
 }
