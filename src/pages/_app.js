b5fce960793845817d551ab0d174eb59c2e7267b
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-04-14 01:34:49 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,14 +5,17 @@ import "@/styles/theme.css";
 import "@/styles/globals.css";
 
 import { AuthProvider } from "@/contexts/AuthContext";
+import { NetworkProvider } from "@/contexts/NetworkContext"; // ✅ PRIDĖTA
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <NetworkProvider> {/* ✅ Pirmiau Auth, kad network galėtų veikti nepriklausomai */}
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </NetworkProvider>
     </AuthProvider>
   );
 }
