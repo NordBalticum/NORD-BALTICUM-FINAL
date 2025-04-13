1a79559910adf85baf19cc2b15afe4c25debd4c7
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-04-13 22:42:56 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,16 @@
 "use client";
 
+// ✅ Import globalių stilių
 import "@/styles/theme.css";
 import "@/styles/globals.css";
 
-import { AuthProvider } from "@/contexts/AuthContext"; // Autentifikacijos kontekstas
-import Layout from "@/components/Layout"; // Pagrindinis Layout
+import { AuthProvider } from "@/contexts/AuthContext";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Layout> {/* Apvyniojame Layout komponentu */}
+      <Layout>
         <Component {...pageProps} />
       </Layout>
     </AuthProvider>
