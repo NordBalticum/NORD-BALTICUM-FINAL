d1f420b395272002cf18e7b0fa53ff536cdf6df0
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-04-15 23:14:05 +0300

    Update history.js

--- src/pages/history.js
+++ src/pages/history.js
@@ -123,7 +123,7 @@ export default function HistoryPage() {
   }
 
   return (
-    <main className={`${styles.container} ${background.gradient}`}>
+    <main className={`${styles.container} ${background.gradient}`} style={{ paddingBottom: "100px" }}>
       <div className={styles.wrapper}>
         <h1 className={styles.title}>Transaction History</h1>
 
