055eaaf124f724ae72d64e5e3de71368540da9d3
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-04-18 01:13:33 +0300

    Create index.js

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+"use client";
+
+export const NETWORKS = {
+  eth: { label: "ETH", min: 0.01, explorer: "https://etherscan.io/tx/" },
+  bnb: { label: "BNB", min: 0.0005, explorer: "https://bscscan.com/tx/" },
+  matic: { label: "MATIC", min: 0.1, explorer: "https://polygonscan.com/tx/" },
+  avax: { label: "AVAX", min: 0.01, explorer: "https://snowtrace.io/tx/" },
+};
