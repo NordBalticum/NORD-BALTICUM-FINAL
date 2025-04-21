26c49be8daafc704b90fc9cc08924f83bef9c5ae
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-04-22 02:50:43 +0300

    Update fallbackRPCs.js

--- src/utils/fallbackRPCs.js
+++ src/utils/fallbackRPCs.js
@@ -1,29 +1,32 @@
 "use client";
 
+/**
+ * 🧱 Fallback RPC sąrašas pagal chainId
+ */
 export const ethersFallbackProviders = {
-  1: [
+  1: [ // Ethereum Mainnet
     "https://rpc.ankr.com/eth",
     "https://eth-mainnet.public.blastapi.io",
     "https://cloudflare-eth.com"
   ],
-  137: [
+  137: [ // Polygon
     "https://polygon-rpc.com",
     "https://rpc.ankr.com/polygon",
     "https://polygon-bor.publicnode.com"
   ],
-  56: [
+  56: [ // Binance Smart Chain
     "https://bsc-dataseed.binance.org",
     "https://rpc.ankr.com/bsc",
     "https://bsc.publicnode.com"
   ],
-  43114: [
+  43114: [ // Avalanche C-Chain
     "https://api.avax.network/ext/bc/C/rpc",
     "https://rpc.ankr.com/avalanche",
     "https://avalanche.publicnode.com"
   ],
-  10: [
+  10: [ // Optimism
     "https://mainnet.optimism.io",
     "https://optimism.publicnode.com",
     "https://rpc.ankr.com/optimism"
-  ]
+  ],
 };
