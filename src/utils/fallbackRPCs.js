227d4e63c9b46b59ea1ab2796ceccd337dc819e0
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-04-26 06:04:38 +0300

    Update fallbackRPCs.js

--- src/utils/fallbackRPCs.js
+++ src/utils/fallbackRPCs.js
@@ -1,166 +1,152 @@
-// src/utils/fallbackRPCs.js
 "use client";
 
+import networks from "@/data/networks";
+
 /**
- * A map of chainId → [RPC URLs…].
- * 1️⃣ Official/public RPC
- * 2️⃣ Your Ankr endpoint (with API key)
- * 3️⃣ PublicNode (or other fallback)
+ * fallback RPC URLs for each chainId
  */
-export const ethersFallbackProviders = {
-  // Ethereum
-  1: [
+
+export const ethersFallbackProviders = {};
+
+const rpcUrls = {
+  eth: [
     "https://eth-mainnet.public.blastapi.io",
     "https://rpc.ankr.com/eth",
     "https://cloudflare-eth.com"
   ],
-  11155111: [
+  sepolia: [
     "https://rpc.sepolia.org",
     "https://rpc.ankr.com/eth_sepolia",
     "https://sepolia.publicnode.com"
   ],
-
-  // Polygon
-  137: [
+  polygon: [
     "https://polygon-rpc.com",
     "https://rpc.ankr.com/polygon",
     "https://polygon-bor.publicnode.com"
   ],
-  80001: [
+  mumbai: [
     "https://matic-mumbai.chainstacklabs.com",
     "https://rpc.ankr.com/polygon_mumbai",
     "https://rpc-mumbai.maticvigil.com"
   ],
-
-  // BNB Chain
-  56: [
+  bnb: [
     "https://bsc-dataseed.binance.org",
     "https://rpc.ankr.com/bsc",
     "https://bsc.publicnode.com"
   ],
-  97: [
+  bnbt: [
     "https://data-seed-prebsc-1-s1.binance.org:8545/",
     "https://rpc.ankr.com/bsc_testnet",
     "https://bsc-testnet.publicnode.com"
   ],
-
-  // Avalanche
-  43114: [
+  avax: [
     "https://api.avax.network/ext/bc/C/rpc",
     "https://rpc.ankr.com/avalanche",
     "https://avalanche.publicnode.com"
   ],
-  43113: [
+  fuji: [
     "https://api.avax-test.network/ext/bc/C/rpc",
     "https://rpc.ankr.com/avalanche_fuji",
     "https://avalanche-fuji.publicnode.com"
   ],
-
-  // Optimism
-  10: [
+  optimism: [
     "https://mainnet.optimism.io",
     "https://rpc.ankr.com/optimism",
     "https://optimism.publicnode.com"
   ],
-  420: [
+  "optimism-goerli": [
     "https://goerli.optimism.io",
     "https://rpc.ankr.com/optimism_goerli",
     "https://optimism-goerli.publicnode.com"
   ],
-
-  // Arbitrum
-  42161: [
+  arbitrum: [
     "https://arb1.arbitrum.io/rpc",
     "https://rpc.ankr.com/arbitrum",
     "https://arbitrum.publicnode.com"
   ],
-  421613: [
+  "arbitrum-goerli": [
     "https://goerli-rollup.arbitrum.io/rpc",
     "https://rpc.ankr.com/arbitrum_goerli",
     "https://arbitrum-goerli.publicnode.com"
   ],
-
-  // Base
-  8453: [
+  base: [
     "https://base-mainnet.public.blastapi.io",
     "https://rpc.ankr.com/base",
     "https://base.publicnode.com"
   ],
-  84531: [
+  "base-goerli": [
     "https://goerli.base.org",
     "https://rpc.ankr.com/base_goerli",
     "https://base-goerli.publicnode.com"
   ],
-
-  // zkSync Era
-  324: [
+  zksync: [
     "https://mainnet.era.zksync.io",
     "https://rpc.ankr.com/zksync",
     "https://zksync.publicnode.com"
   ],
-  280: [
+  "zksync-testnet": [
     "https://testnet.era.zksync.dev",
     "https://rpc.ankr.com/zksync_testnet",
     "https://zksync-testnet.publicnode.com"
   ],
-
-  // Linea
-  59144: [
+  linea: [
     "https://rpc.linea.build",
     "https://rpc.ankr.com/linea",
     "https://linea.publicnode.com"
   ],
-  59140: [
+  "linea-testnet": [
     "https://rpc.goerli.linea.build",
     "https://rpc.ankr.com/linea_goerli",
     "https://linea-goerli.publicnode.com"
   ],
-
-  // Scroll
-  534352: [
+  scroll: [
     "https://scroll.io/l2",
     "https://rpc.ankr.com/scroll",
     "https://scroll.publicnode.com"
   ],
-  534353: [
+  "scroll-testnet": [
     "https://scroll-testnet.public.blastapi.io",
     "https://rpc.ankr.com/scroll_testnet",
     "https://scroll-testnet.publicnode.com"
   ],
-
-  // Mantle
-  5000: [
+  mantle: [
     "https://rpc.mantle.xyz",
     "https://rpc.ankr.com/mantle",
     "https://mantle.publicnode.com"
   ],
-  5001: [
+  "mantle-testnet": [
     "https://rpc.testnet.mantle.xyz",
     "https://rpc.ankr.com/mantle_testnet",
     "https://mantle-testnet.publicnode.com"
   ],
-
-  // Celo
-  42220: [
+  celo: [
     "https://forno.celo.org",
     "https://rpc.ankr.com/celo",
     "https://celo.publicnode.com"
   ],
-  44787: [
+  alfajores: [
     "https://alfajores-forno.celo-testnet.org",
     "https://rpc.ankr.com/celo_alfajores",
     "https://alfajores.publicnode.com"
   ],
-
-  // Gnosis Chain
-  100: [
+  gnosis: [
     "https://rpc.gnosischain.com",
     "https://rpc.ankr.com/xdai",
     "https://gnosis.publicnode.com"
   ],
-  10200: [
+  chiado: [
     "https://rpc.chiadochain.net",
     "https://rpc.ankr.com/gnosis_chiado",
     "https://chiado.publicnode.com"
   ]
 };
+
+// Automatinis sujungimas
+for (const network of networks) {
+  if (network.chainId && rpcUrls[network.value]) {
+    ethersFallbackProviders[network.chainId] = rpcUrls[network.value];
+  }
+  if (network.testnet && rpcUrls[network.testnet.value]) {
+    ethersFallbackProviders[network.testnet.chainId] = rpcUrls[network.testnet.value];
+  }
+}
