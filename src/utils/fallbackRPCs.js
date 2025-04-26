c626c9fc2e5a76302801a9323885e6ef04205628
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-04-26 03:17:37 +0300

    Update fallbackRPCs.js

--- src/utils/fallbackRPCs.js
+++ src/utils/fallbackRPCs.js
@@ -1,32 +1,29 @@
 // src/utils/fallbackRPCs.js
 "use client";
 
-/**
- * 🧱 Fallback RPC endpoints by chainId
- */
 export const ethersFallbackProviders = {
   1: [ // Ethereum Mainnet
     "https://rpc.ankr.com/eth",
     "https://eth-mainnet.public.blastapi.io",
     "https://cloudflare-eth.com"
   ],
-  137: [ // Polygon Mainnet
-    "https://polygon-rpc.com",
-    "https://rpc.ankr.com/polygon",
-    "https://polygon-bor.publicnode.com"
-  ],
   56: [ // BSC Mainnet
     "https://bsc-dataseed.binance.org",
     "https://rpc.ankr.com/bsc",
     "https://bsc.publicnode.com"
   ],
+  97: [ // BSC Testnet (tbnb)
+    "https://data-seed-prebsc-1-s1.binance.org:8545/",
+    "https://bsc-testnet.public.blastapi.io"
+  ],
+  137: [ // Polygon Mainnet (matic)
+    "https://polygon-rpc.com",
+    "https://rpc.ankr.com/polygon",
+    "https://polygon-bor.publicnode.com"
+  ],
   43114: [ // Avalanche C-Chain
     "https://api.avax.network/ext/bc/C/rpc",
     "https://rpc.ankr.com/avalanche",
     "https://avalanche.publicnode.com"
-  ],
-  97: [ // BSC Testnet
-    "https://data-seed-prebsc-1-s1.binance.org:8545/",
-    "https://bsc-testnet.public.blastapi.io"
   ],
 };
