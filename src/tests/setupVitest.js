36f64c8bfae480f2c5c318b04c7af55353542795
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-05-07 09:38:51 +0300

    Create setupVitest.js

---
+++ src/tests/setupVitest.js
@@ -0,0 +1,3 @@
+process.env.NEXT_PUBLIC_ENCRYPTION_SECRET = 'testtesttesttest1234567890';
+process.env.NEXT_PUBLIC_ADMIN_WALLET = '0x000000000000000000000000000000000000dead';
+process.env.ADMIN_EMAIL = 'admin@nordbalticum.io';
