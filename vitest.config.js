0e391cbe71661e5c4cfc40acc55fd22e75dbe378
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-05-07 09:39:05 +0300

    Update vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    include: ['src/tests/**/*.test.js'],
+    exclude: ['node_modules', 'dist'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: './coverage',
+      exclude: ['src/scripts', '**/*.config.js'],
+    },
+    setupFiles: ['src/tests/setupVitest.js']
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
