92e80b1f4ed763cb1a779e614392ba2ac1d26cfb
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-05-09 05:44:42 +0300

    Update SkeletonLoader.js

--- src/components/SkeletonLoader.js
+++ src/components/SkeletonLoader.js
@@ -2,14 +2,21 @@
 
 import styles from "@/components/skeleton.module.css";
 
-export default function SkeletonLoader({ width = "100%", height = "20px", radius = "12px" }) {
+export default function SkeletonLoader({
+  width = "100%",
+  height = "20px",
+  radius = "12px",
+  style = {},
+  className = "",
+}) {
   return (
     <div
-      className={styles.skeleton}
+      className={`${styles.skeleton} ${className}`}
       style={{
         width,
         height,
         borderRadius: radius,
+        ...style,
       }}
     />
   );
