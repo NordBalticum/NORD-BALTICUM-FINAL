b8dca48b63c9a32495f772807bcc5ef186e76151
NordBalticum <200170815+NordBalticum@users.noreply.github.com>
2025-05-09 04:59:02 +0300

    Update MiniLoadingSpinner.js

--- src/components/MiniLoadingSpinner.js
+++ src/components/MiniLoadingSpinner.js
@@ -4,16 +4,19 @@ import styles from "./miniloadingspinner.module.css"; // Import CSS module
 
 export default function MiniLoadingSpinner({ size = 30, color = "#3498db", borderColor = "#f3f3f3" }) {
   return (
-    <div className={styles.spinnerWrapper}>
-      <div
-        className={styles.spinner}
-        style={{
-          width: size,
-          height: size,
-          border: `3px solid ${borderColor}`,
-          borderTop: `3px solid ${color}`,
-        }}
-      ></div>
+    <div className={styles.fullscreenOverlay}>
+      <div className={styles.spinnerWrapper}>
+        <div
+          className={styles.spinner}
+          style={{
+            width: size,
+            height: size,
+            border: `3px solid ${borderColor}`,
+            borderTop: `3px solid ${color}`,
+          }}
+        ></div>
+        <div className={styles.loadingMessage}>Loading...</div>
+      </div>
     </div>
   );
 }
